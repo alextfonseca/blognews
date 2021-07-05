90bd2d185e7a8774c8b4fd56f993b4a41ec3821a
Eduardo Nogueira <eduardodudunog@gmail.com>
2021-07-05 20:45:14 -0300

    Adicionada a funcionlidade de filtrar as notícias pelas fontes

--- src/pages/newsList.tsx
+++ src/pages/newsList.tsx
@@ -1,21 +1,73 @@
 import Head from "next/head";
 import Link from "next/link";
+import Router from "next/router";
+import { useEffect } from "react";
+
 import styles from "./newsList.module.scss";
 
-export default function newsList(props) {
-  const newsArray = props.articles;
+interface NewListProps {
+  sources: Source[];
+  data: {
+    articles: Article[];
+  };
+}
+
+interface Article {
+  source: {
+    id: string;
+    name: string;
+  };
+  title: string;
+  description: string;
+  url: string;
+  publishedAt: Date;
+}
+
+interface Source {
+  id?: string;
+  name?: string;
+}
+
+export default function newsList(props: NewListProps) {
+  const newsArray = props.data.articles;
+  const sources = props.sources;
+
+  function changeSource() {
+    const data = document.getElementById("source") as HTMLSelectElement;
+
+    Router.push(`/newsList/${data.value}`);
+  }
+
+  useEffect(() => {
+    const data = document.getElementById("source") as HTMLSelectElement;
+    data.value = "source";
+  }, []);
+
   return (
     <>
       <Head>
         <title>Noticias | blognews</title>
       </Head>
+      <div className={styles.filter}>
+        <label htmlFor="">Filtre por</label>
+
+        <select name="" id="source" onChange={changeSource}>
+          <option value="source">Fonte</option>
+          {sources.map(source => {
+            return <option value={source.id}>{source.name}</option>;
+          })}
+        </select>
+      </div>
       <section className={styles.content} id="topo">
-        {newsArray.map((news) => {
+        {newsArray.map(news => {
           return (
             <div key={news.title} className={styles.newsList}>
-              <span>
-                {new Intl.DateTimeFormat("pt-br").format(news.createdAt)}
-              </span>
+              <div>
+                <span>
+                  {new Intl.DateTimeFormat("pt-br").format(news.createdAt)}
+                </span>
+                <span>{news.source.name}</span>
+              </div>
               <Link href={news.url}>
                 <a>{news.title}</a>
               </Link>
@@ -30,14 +82,28 @@ export default function newsList(props) {
 
 export async function getStaticProps() {
   const response = await fetch(
-    "https://newsapi.org/v2/top-headlines?sources=google-news-br&apiKey=f076cd3a334e415cb74ea093d5eb9833",
+    "https://newsapi.org/v2/top-headlines?country=br&apiKey=f076cd3a334e415cb74ea093d5eb9833"
   );
 
   const data = await response.json();
 
+  const sourcesData = await fetch(
+    "https://newsapi.org/v2/top-headlines/sources?country=br&apiKey=f076cd3a334e415cb74ea093d5eb9833"
+  ).then(result => {
+    return result.json();
+  });
+
+  const sources = sourcesData.sources.map(source => {
+    return {
+      id: source.id,
+      name: source.name,
+    };
+  });
+
   return {
     props: {
-      articles: data.articles,
+      sources: sources,
+      data: data,
     },
     revalidate: 60 * 60,
   };
---
+++ src/pages/newsList/[source].tsx
@@ -0,0 +1,148 @@
+import { useEffect } from "react";
+import Head from "next/head";
+import Link from "next/link";
+import Router from "next/router";
+import { GetStaticPaths, GetStaticProps } from "next";
+
+import styles from "../newsList.module.scss";
+
+interface CategoryProps {
+  source?: string;
+  sources: Source[];
+  data: {
+    articles: Article[];
+  };
+}
+
+interface Article {
+  source: {
+    id: string;
+    name: string;
+  };
+  title: string;
+  description: string;
+  url: string;
+  publishedAt: Date;
+}
+
+interface Source {
+  id?: string;
+  name?: string;
+}
+
+export default function Source(props: CategoryProps) {
+  const newsArray = props.data.articles;
+  const sources = props.sources;
+
+  async function changeSource() {
+    const source = document.getElementById("source") as HTMLSelectElement;
+
+    Router.push(`/newsList/${source.value}`);
+  }
+
+  useEffect(() => {
+    const source = document.getElementById("source") as HTMLSelectElement;
+
+    source.value = props.source;
+  }, []);
+
+  return (
+    <>
+      <Head>
+        <title>Noticias | blognews</title>
+      </Head>
+      <div className={styles.filter}>
+        <label htmlFor="">Filtre por</label>
+
+        <select name="" id="source" onChange={changeSource}>
+          <option value="source">Todos</option>
+          {sources.map(source => {
+            return <option value={source.id}>{source.name}</option>;
+          })}
+        </select>
+      </div>
+      <section className={styles.content} id="topo">
+        {newsArray.length > 0 ? (
+          newsArray.map(news => {
+            return (
+              <div key={news.title} className={styles.newsList}>
+                <div>
+                  <span>
+                    {new Intl.DateTimeFormat("pt-br").format(news.createdAt)}
+                  </span>
+                </div>
+                <Link href={news.url}>
+                  <a>{news.title}</a>
+                </Link>
+                <p>{news.description}</p>
+              </div>
+            );
+          })
+        ) : (
+          <p>Categoria inexistente</p>
+        )}
+      </section>
+    </>
+  );
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const sourcesData = await fetch(
+    "https://newsapi.org/v2/top-headlines/sources?country=br&apiKey=f076cd3a334e415cb74ea093d5eb9833"
+  ).then(result => {
+    return result.json();
+  });
+
+  const paths = sourcesData.sources.map(source => {
+    return {
+      params: {
+        source: source.id,
+      },
+    };
+  });
+
+  return {
+    paths,
+    fallback: "blocking",
+  };
+};
+
+export const getStaticProps: GetStaticProps = async ctx => {
+  const { source } = ctx.params;
+
+  const sourcesData = await fetch(
+    "https://newsapi.org/v2/top-headlines/sources?country=br&apiKey=f076cd3a334e415cb74ea093d5eb9833"
+  ).then(result => {
+    return result.json();
+  });
+
+  var response;
+  var data = {};
+
+  const sourcesID = sourcesData.sources.map(source => {
+    return source.id;
+  });
+
+  if (sourcesID.includes(source)) {
+    response = await fetch(
+      `https://newsapi.org/v2/top-headlines?sources=${source}&apiKey=f076cd3a334e415cb74ea093d5eb9833`
+    );
+
+    data = await response.json();
+  } else {
+    response = await fetch(
+      "https://newsapi.org/v2/top-headlines?country=br&apiKey=f076cd3a334e415cb74ea093d5eb9833"
+    );
+
+    data = await response.json();
+  }
+
+  return {
+    props: {
+      source: source,
+      sources: sourcesData.sources,
+      data: data,
+    },
+    revalidate: 60 * 60,
+  };
+};
