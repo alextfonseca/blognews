200a5e3367f76a7543b48064313200ca5cfdbf61
Eduardo Nogueira <eduardodudunog@gmail.com>
2021-11-02 15:27:02 -0300

    Adicionado um loading para melhorar o UX da plataforma

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,43 @@
-import "../../styles/global.scss";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
 import { Header } from "../components/Header";
 
+import ReactLoading from 'react-loading';
+
+import "../../styles/global.scss";
+
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+  const [loading, setLoading] = useState(false);
+  
+  useEffect(() => {
+    let isMounted = true;
+
+    const handleStart = (url) => {
+      url !== router.pathname ? setLoading(true) : setLoading(false);
+    };
+
+    const handleComplete = () => {
+      setLoading(false);
+    }
+
+    router.events.on("routeChangeStart", handleStart);
+    router.events.on("routeChangeComplete", handleComplete);
+
+    return () => { isMounted = false };
+  }, []);
+
   return (
     <>
       <Header />
-      <Component {...pageProps} />
+      
+      { loading ? (
+        <div className="bodyLoading">
+          <ReactLoading type={"spin"} color={"#fff"} />
+        </div>
+      ) : (
+        <Component {...pageProps} />
+      )}
     </>
   );
 }
